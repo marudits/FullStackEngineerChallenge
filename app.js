f15613d31733c596d45cd7b87a2d8e985be23eb1
marudits <marudits@gmail.com>
2019-06-11 14:35:04 +0700

    :sparkles: NEW FEATURES | Route express to front-end

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', express.static(path.join(__dirname, 'front-end', 'dist')));
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
